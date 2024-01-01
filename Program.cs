9d4acbe7d08bb996234182d21b7b8f2563a73bc6
Duckster Boo <ibrahimkhan1234_5@icloud.com>
2024-01-01 18:08:36 +0000

    merge

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ using Raylib_cs;
 using System.Numerics;
 using static Raylib_cs.Raylib;
 using Vulkan;
+using Veldrid;
 
 namespace HelloWorld;
 
@@ -41,18 +42,164 @@ class Program : Overlay
 
         Raylib.InitWindow(800, 480, "Fahoom Projectile Motion Renderer");
 
-        Camera3D camera = new();
-        camera.Position = new Vector3(10f, 5f, 0f);
-        camera.Target = new Vector3(0f, 0f, 0f);
-        camera.Up = new Vector3(0.0f, 1.0f, 0.0f);
-        camera.FovY = 45.0f;
-        camera.Projection = CameraProjection.CAMERA_PERSPECTIVE;
+        float camX = 0f;
+        float camY = 0f;
+        float camZ = 50f;
+
+        //models
+        Model sphere = LoadModelFromMesh(GenMeshSphere(1.0f, 25, 25));
+        Model nullsphere = LoadModelFromMesh(GenMeshSphere(0f, 25, 25));
+        Model plane = LoadModelFromMesh(GenMeshSphere(1.0f, 25, 25));
+        Model lineSphere = LoadModelFromMesh(GenMeshSphere(0.25f, 25, 25));
+
+        /*
+        //shaders
+        Shader shader = LoadShader(
+            "lighting.vs",
+            "lighting.fs"
+        );
+        */
+        //fps setting
+        SetTargetFPS(60);
 
 
 
         while (!Raylib.WindowShouldClose())
         {
-            program.RayLib(camera);
+            //Camera
+            Camera3D camera = new();
+            camera.Position = new Vector3(camX, camY, camZ);
+            camera.Target = new Vector3(0.0f, 0.0f, 0.0f);      // Camera looking at point
+            camera.Up = new Vector3(0.0f, 1.0f, 0.0f);          // Camera up vector (rotation towards target)
+            camera.FovY = 60.0f;                                // Camera field-of-view Y  
+            camera.Projection = CameraProjection.CAMERA_PERSPECTIVE;
+
+            //Camera Controls
+            //X Movement
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_ONE))
+            {
+                camX = camX + 1f;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_TWO))
+            {
+                camX = camX - 1f;
+            }
+            //Y Movement
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_THREE))
+            {
+                camY = camY + 1f;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_FOUR))
+            {
+                camY = camY - 1f;
+            }
+            //Z Movement
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_FIVE))
+            {
+                camZ = camZ + 1f;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_SIX))
+            {
+                camZ = camZ - 1f;
+            }
+            //Reset Camera Position
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_R))
+            {
+                camX = -50f;
+                camY = 0f;
+                camZ = 0f;
+            }
+
+            //vars
+            float gravac = -9.81f;
+            float speed = 17f;
+            float vspeed;
+            float hspeed;
+            float degAngle = 45f; //angle to the vertical, e.g. straight up is 0, horizontal is 90
+            float radAngle = degAngle * 1 / 180 * (float)Math.PI;
+            float t = 0;
+            double posX;
+            double posY;
+            double deltaTime = GetTime();
+            deltaTime = deltaTime - t;
+
+            //calc
+            //s = ut + 1/2(a(t^2))
+            vspeed = speed * (float)Math.Cos(radAngle);
+            hspeed = speed * (float)Math.Sin(radAngle);
+
+            posY = vspeed * deltaTime + 0.5 * gravac * deltaTime * deltaTime;
+            posX = deltaTime * hspeed;
+
+            //positioning
+            float vecY = (float)posY;
+            float vecX = (float)posX;
+
+            Vector3 vecsphere = new Vector3(vecX, vecY, 0);
+            Vector3 vecplane = new Vector3(0f, -5f, 0f);
+
+            //collision
+            bool collision = false;
+
+            collision = CheckCollisionSpheres(vecsphere, 0.1f, vecplane, 0.1f);
+            if (collision)
+            {
+                Raylib.DrawText("HIT", 50, 50, 50, Color.BLACK);
+            }
+
+            //Vector Positions
+            Vector3 StartPos = new Vector3(0, 0, 0);
+            Vector3 CurrentPos = new Vector3(vecX, vecY, 0);
+
+            //displacement
+            Vector3 displacementvec = CurrentPos - StartPos;
+            float displacement = (float)Math.Sqrt(displacementvec.Y * displacementvec.Y + displacementvec.Z * displacementvec.Z);
+
+            //hide cursor
+            //DisableCursor();
+
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.WHITE);
+
+            Raylib.BeginMode3D(camera);
+
+            if (program.var1 == true)
+            {
+                Raylib.DrawSphere(new Vector3(0, 0, 0), 1, Color.GREEN); //test enable object in raylib via imgui
+            }
+
+            //Grid
+            DrawGrid(200, 1);
+
+            //Objects
+            DrawModel(sphere, vecsphere, 1.0f, Color.BLUE);
+            DrawModel(plane, vecplane, 1.0f, Color.RED);
+
+
+            //Lines
+            DrawLine3D(StartPos, CurrentPos, Color.GREEN);
+
+
+            Raylib.EndMode3D();
+            
+            float screeny = GetScreenHeight() / 2;
+            float screenx = GetScreenWidth() / 2;
+
+            //DrawCircle((int)screenx, (int)screeny, 5, Color.GREEN);
+            DrawCircle((int)screenx, (int)screeny, 5, Color.GREEN);
+
+            Vector2 k = GetWorldToScreen(CurrentPos, camera);
+
+            //Raylib.DrawText("Fahoom Projectile Motion Renderer", 20,  10, 20, Color.BLACK);
+            Raylib.DrawText(k.ToString(), 20, 10, 20, Color.BLACK);
+            Raylib.DrawText("X Vector: " + vecX.ToString(), 20, 40, 20, Color.BLACK);
+            Raylib.DrawText("Y Vector: " + vecY.ToString(), 20, 70, 20, Color.BLACK);
+            Raylib.DrawText("Displacement: " + displacement, 20, 100, 20, Color.BLACK);
+
+            DrawFPS(510, 10);
+
+            Raylib.EndDrawing();
         }
         
         Raylib.CloseWindow();
@@ -73,25 +220,6 @@ class Program : Overlay
             Environment.Exit(0);
         }
         ImGui.End();
-    }
-    void RayLib(Camera3D camera) //raylib
-    {
-        Raylib.BeginDrawing();
-
-        Raylib.ClearBackground(Color.WHITE);
-
-        Raylib.BeginMode3D(camera);
-
-        Raylib.DrawGrid(50, 1);
-
-        if (var1 == true)
-        {
-            Raylib.DrawSphere(new Vector3(0, 0, 0), 1, Color.GREEN); //test enable object in raylib via imgui
-        }
-
-        Raylib.EndMode3D();
-
-        Raylib.EndDrawing();
     }
 
 }
